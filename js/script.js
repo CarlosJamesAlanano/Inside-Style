ca66b7b14ca04e67cd9306ccd4a894db4e3f6d42
CarlosJamesAlanano <cjalanano.dev@gmail.com>
2025-03-26 15:56:09 +0800

    Add backdrop functionality for mobile navigation toggle

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const toggleMenu = document.querySelector(".toggle_menu");
 const navMobile = document.querySelector(".nav");
+const backdrop = document.querySelector(".backdrop");
 
 toggleMenu.addEventListener("click", ()=>{
     toggleMenu.classList.toggle("open");
     navMobile.classList.toggle("open");
+    backdrop.classList.toggle("open");
 })
